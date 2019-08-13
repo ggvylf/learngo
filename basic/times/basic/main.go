8885a75777298aba5133b02f445e3d29a18c7b25
ggvylf <ggvylf@163.com>
2019-08-14 01:21:31 +0800

    first

--- basic/times/basic/main.go
+++ basic/times/basic/main.go
@@ -5,12 +5,14 @@ import (
 	"time"
 )
 
-func main() {
-
+func CurrentTime() {
 	//获取当前时间
 	now := time.Now()
 	fmt.Printf("当前时间为：%v\n", now)
+}
 
+func ShowTime() {
+	now := time.Now()
 	//当前的年月日时分秒
 	fmt.Println(now.Year())
 	fmt.Println(now.Month())
@@ -18,15 +20,21 @@ func main() {
 	fmt.Println(now.Hour())
 	fmt.Println(now.Minute())
 	fmt.Println(now.Second())
+}
 
+func TimeStamps() {
+	now := time.Now()
 	//时间戳，返回从1970.01.01到现在的秒数
 	fmt.Printf("时间戳：%v 秒\n", now.Unix())
 	fmt.Printf("时间戳：%v 纳秒\n", now.UnixNano())
 
-	//把时间戳抓暖胃具体的时间格式
+	//把时间戳转换成胃具体的时间格式
 	t := time.Unix(1565689034, 0)
 	fmt.Println("时间戳转时间格式：", t)
 
+}
+
+func TimeSleep() {
 	//常见的const
 	//time.Duration 一纳秒
 	//time.Nanosecond 纳秒，等同于1 time.Duration
@@ -42,11 +50,47 @@ func main() {
 	n := 5
 	time.Sleep(time.Duration(n) * time.Nanosecond)
 
+}
+
+func FormatOutput() {
+	now := time.Now()
+	//格式化输出，根据参考时间的样式进行输出
+	//格式化初始时间：2006-01-02 15:04:05，这也是go诞生的时间
+	fmt.Println(now.Format("2006-01-02 15-04"))
+	fmt.Println(now.Format("2006/01/02 15-04-00"))
+	fmt.Println(now.Format("02-01/2006 03-04-00 PM"))
+
 	//格式化输出
 	fmt.Printf("%02d-%02d-%02d %02d:%02d:%02d\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+}
 
-	//格式化输出，根据参考时间的样式进行输出
-	//格式化初始时间：2016-01-02 15:04:00，这也是go诞生的时间
-	fmt.Println(now.Format("2006-01-02 15-04"))
-	fmt.Println(now.Format("02-01/2016 15-04-00"))
+func ParseTime() {
+	//解析字符串类型的时间
+	timestr := "2019-08-14 01:02:03"
+
+	//制定格式：
+	timeformat := "2006-01-02 15:04:05"
+
+	//指定时区
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//不根据时区解析，默认使用UTC时间
+	t, err := time.Parse(timeformat, timestr)
+	fmt.Println("解析结果=", t)
+
+	//根据时区解析
+	t, err = time.ParseInLocation(timeformat, timestr, loc)
+	fmt.Println("解析结果=", t)
+}
+
+func main() {
+	CurrentTime()
+	ShowTime()
+	TimeStamps()
+	TimeSleep()
+	FormatOutput()
+	ParseTime()
 }
--- basic/times/timeoprate/main.go
+++ basic/times/timeoprate/main.go
@@ -5,17 +5,23 @@ import (
 	"time"
 )
 
-func main() {
+func AddTime() {
 	//时间+时间间隔
 	t := time.Now()
 	t2 := t.Add(time.Hour)
 	fmt.Println("当前时间t=", t)
 	fmt.Println("增加一小时后，t2=", t2)
+}
 
+func SubTime() {
+	t := time.Now()
+	t2 := t.Add(time.Hour)
 	//时间之差
 	t3 := t.Sub(t2)
 	fmt.Println("t2-t时间之差=", t3)
+}
 
+func EqualTime() {
 	//判断时间是否相等。会考虑时区的影响，不同时区的标准时间爱你也可以比较。如果直接判断==，则还会比较时区和地点
 	//定义时区偏移量
 	secondsEastOfUTC := int((8 * time.Hour).Seconds())
@@ -34,14 +40,18 @@ func main() {
 	fmt.Printf("直接使用==比较的结果 = %v\n", datesEqualUsingEqualOperator)
 	fmt.Printf("使用Equal比较的结果 = %v\n", datesEqualUsingFunction)
 
+}
+
+func BeforeAndAfter() {
 	u := time.Now()
-	fmt.Println(u)
-	fmt.Println(t2)
+	t2 := u.Add(time.Hour)
+	fmt.Println("u=", u)
+	fmt.Println("t2=", t2)
 	//比较时间点，如果t2表示的时间点在u之前，返回真
 	if t2.Before(u) {
-		fmt.Println("u前t2后")
-	} else {
 		fmt.Println("t2前u后")
+	} else {
+		fmt.Println("u前t2后")
 	}
 
 	//比较时间点，如果t2表示的时间点在u之后，返回真
@@ -50,5 +60,34 @@ func main() {
 	} else {
 		fmt.Println("t2前u后")
 	}
+}
 
+func TimeTick() {
+	//定时器，返回值是一个chan
+	//循环输出chan中的内容
+	i := 0
+	//timer:=time.NewTicker(time.Second)
+	//for {
+	//	time:=<-timer.C
+	//	fmt.Println(time)
+	//	i++
+	//	if i==10 {
+	//		return
+	//	}
+	//}
+	for time := range time.Tick(time.Second) {
+		fmt.Println(time)
+		i++
+		if i == 10 {
+			return
+		}
+	}
+}
+
+func main() {
+	AddTime()
+	SubTime()
+	EqualTime()
+	BeforeAndAfter()
+	TimeTick()
 }
