c3251f6baabcf4d78dd9db992fb108439fb1c897
ggvylf <ggvylf@163.com>
2019-08-16 01:12:42 +0800

    first

--- basic/reflects/reflectstruct/main.go
+++ basic/reflects/reflectstruct/main.go
@@ -10,6 +10,44 @@ type Student struct {
 	Score int    `json:"score"`
 }
 
+func (s Student) Study(a, b int) {
+	str := "is study"
+	fmt.Println(str)
+	fmt.Println(a + b)
+
+}
+
+func (s Student) Sleep(a, b string) {
+	str := "is sleeping"
+	fmt.Println(str)
+	fmt.Println(a + b)
+}
+
+func PrintMethod(x interface{}) {
+	xtype := reflect.TypeOf(x)
+	xvalue := reflect.ValueOf(x)
+	//获取方法个数
+	fmt.Println("结构体包含的方法个数=", xtype.NumMethod())
+
+	//遍历所有的方法名称和类型，这里要使用值的反射，才能获取到具体的方法来调用
+	for i := 0; i < xvalue.NumMethod(); i++ {
+		methodtype := xvalue.Method(i).Type()
+		fmt.Printf("结构体中方法的名称=%v，方法的类型=%v\n", xtype.Method(i).Name, methodtype)
+
+	}
+
+	args := []reflect.Value{}
+	strargs := []reflect.Value{}
+	args = append(args, reflect.ValueOf(1))
+	args = append(args, reflect.ValueOf(3))
+	strargs = append(strargs, reflect.ValueOf("2"))
+	strargs = append(strargs, reflect.ValueOf("4"))
+	//调用方法
+	xvalue.Elem().Method(0).Call(args)
+	xvalue.Elem().Method(1).Call(strargs)
+
+}
+
 func main() {
 	s1 := Student{"tom", 99}
 	stureftype := reflect.TypeOf(s1)
@@ -26,8 +64,10 @@ func main() {
 		//取对应tag的值
 		fmt.Printf("tag为json的值=%v,tag为xml的值=%v\n", obj.Tag.Get("json"), obj.Tag.Get("xml"))
 	}
-	//根据字段名字取值
-	v, _ := stureftype.FieldByName("Score")
-	fmt.Println("Score字段的值=")
+	//根据字段名字查找对应字段
+	s1score, _ := stureftype.FieldByName("Score")
+	fmt.Println("s1的Score字段=", s1score)
+
+	PrintMethod(s1)
 
 }
