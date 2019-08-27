6078f042a49734013a23217f466637519fd39ac0
ggvylf <ggvylf@163.com>
2019-08-27 19:20:44 +0800

    first

--- basic/tests/main.go
+++ basic/tests/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("name\tage\tscore")
+	str1 := "hello呵呵呵"
+	s1 := []rune(str1)
+	s2 := make([]rune, 10)
+	for _, v := range s1 {
+		l := len(string(v))
+		switch l {
+		case 1:
+			fmt.Println("字母=", string(v))
+		default:
+			fmt.Println("汉字=", string(v))
+			s2 = append(s2, v)
+		}
+	}
+	fmt.Println("s2=", string(s2))
 }
