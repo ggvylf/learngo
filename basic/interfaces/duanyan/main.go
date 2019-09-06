3a8e944db5519e00986d24de3537d4b34995cdb3
ggvylf <ggvylf@163.com>
2019-09-06 23:41:23 +0800

    first

--- basic/interfaces/duanyan/main.go
+++ basic/interfaces/duanyan/main.go
@@ -3,6 +3,21 @@ package main
 
 import "fmt"
 
+type Coder struct{}
+
+func (c *Coder) Work() {
+	fmt.Println("is working")
+}
+
+func (c *Coder) Rest() {
+	fmt.Println("is resting")
+}
+
+type Worker interface {
+	Work()
+	Rest()
+}
+
 func Test(a interface{}) {
 	//断言，a是个int类型，如果不是int类型就走if流程
 	b, ok := a.(int)
@@ -30,9 +45,29 @@ func testType(items ...interface{}) {
 	}
 }
 
+func ptrduanyan() {
+
+	//接口的实例只能使用指针
+	var worker Worker
+	c := new(Coder)
+	worker = c
+
+	//注意这里，对借口类型最断言，只能断言为实例或者是指针中的一种，否则编译器会报错。
+	switch worker.(type) {
+	//case Coder :
+	//	fmt.Println("type is *Coder")
+	case *Coder:
+		fmt.Println("type is Coder")
+	}
+
+}
+
 func main() {
 	var a int
+
 	Test(a)
 
 	testType("123", 123, 46)
+
+	ptrduanyan()
 }
--- basic/tests/main.go
+++ basic/tests/main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
+type Coder struct{}
+
+func (c *Coder) Work() {
+	fmt.Println("is working")
+}
+
+func (c *Coder) Rest() {
+	fmt.Println("is resting")
+}
 func main() {
-	str1 := "hello呵呵呵"
-	s1 := []rune(str1)
-	s2 := make([]rune, 10)
-	for _, v := range s1 {
-		l := len(string(v))
-		switch l {
-		case 1:
-			fmt.Println("字母=", string(v))
-		default:
-			fmt.Println("汉字=", string(v))
-			s2 = append(s2, v)
-		}
+
+	var worker Worker
+	c := new(Coder)
+	worker = c
+
+	switch worker.(type) {
+	case Coder:
+		fmt.Println("type is *Coder")
+	case *Coder:
+		fmt.Println("type is Coder")
 	}
-	fmt.Println("s2=", string(s2))
+
 }
