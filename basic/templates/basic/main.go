8270aa303e254becd9746609b154ca205410b194
ggvylf <ggvylf@163.com>
2019-12-24 18:50:19 +0800

    template

---
+++ basic/templates/basic/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	//解析模板
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("template parse failed,err=", err)
+		return
+	}
+
+	//渲染
+	str := "World"
+	err = t.Execute(w, str)
+	if err != nil {
+		fmt.Println("template exec failed err=", err)
+	}
+
+}
+
+func main() {
+	//创建http服务
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		fmt.Println("http server start failed,err=", err)
+		return
+	}
+
+}
