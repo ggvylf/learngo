0e8180c6cc7b20dc2be666b33309cf4792b469d0
ggvylf <ggvylf@163.com>
2020-03-27 23:24:34 +0800

    first

---
+++ basic/templates/editdelies/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func mydelies(w http.ResponseWriter, h *http.Request) {
+	t, err := template.New("base.tmpl").Delims("{[", "]}").ParseFiles("./base.tmpl")
+	if err != nil {
+		fmt.Println("err=", err)
+		return
+	}
+	msg := "hello"
+	t.Execute(w, msg)
+
+}
+
+func main() {
+	http.HandleFunc("/", mydelies)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		fmt.Println("err=", err)
+		return
+	}
+}
