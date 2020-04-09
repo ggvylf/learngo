cd94cf6053812f4ddd30f7307d231710c31594ef
ggvylf <ggvylf@163.com>
2020-04-09 22:56:17 +0800

    gin route

---
+++ ginstudy/templates/location/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	//HTTP重定向
+	r.GET("/index", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "https://www.baidu.com")
+	})
+
+	//路由重定向
+	r.GET("/a", func(c *gin.Context) {
+		//改写请求的uri
+		c.Request.URL.Path = "/b"
+		//路由继续处理后续请求
+		r.HandleContext(c)
+	})
+
+	r.GET("/b", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "this is b page",
+		})
+	})
+
+	r.Run(":8888")
+}
---
+++ ginstudy/templates/router/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	//没有匹配的到路由
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "not route not found",
+		})
+	})
+
+	//指定某个url的全部方法
+	r.Any("/test", func(c *gin.Context) {
+
+		//用switch case来处理
+		switch c.Request.Method {
+		//这里直接使用http定影好的const
+		case http.MethodGet:
+			{
+				c.JSON(http.StatusOK, gin.H{
+					"message": "method get",
+				})
+			}
+		case http.MethodPost:
+			{
+				c.JSON(http.StatusOK, gin.H{
+					"message": "method post",
+				})
+			}
+		case http.MethodPut:
+			{
+				c.JSON(http.StatusOK, gin.H{
+					"message": "method put",
+				})
+			}
+		case http.MethodDelete:
+			{
+				c.JSON(http.StatusOK, gin.H{
+					"message": "method delete",
+				})
+			}
+		}
+	})
+
+	//group route
+	video := r.Group("/video")
+	{
+		video.GET("/login", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "/video/login",
+			})
+		})
+
+		video.GET("/order", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "/video/order",
+			})
+		})
+	}
+
+	book := r.Group("/book")
+	{
+		book.GET("/login", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "/book/login",
+			})
+		})
+
+		book.GET("/order", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "/book/order",
+			})
+		})
+	}
+
+	r.Run(":8888")
+}
