f21bbb2db09d9f1d547bb841b05b2f6ed15c957a
ggvylf <ggvylf@163.com>
2020-10-22 20:05:15 +0800

    context

--- basic/contexts/basic/main.go
+++ basic/contexts/basic/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -31,30 +32,30 @@ import (
 // }
 
 // 方法2 使用channel
-var wg sync.WaitGroup
-var exitChan = make(chan bool)
+// var wg sync.WaitGroup
+// var exitChan = make(chan bool)
 
-func f() {
-	defer wg.Done()
-	for {
-		fmt.Println("hehe")
-		time.Sleep(500 * time.Millisecond)
-		select {
-		case <-exitChan:
-			break
-		default:
-		}
-	}
+// func f() {
+// 	defer wg.Done()
+// 	for {
+// 		fmt.Println("hehe")
+// 		time.Sleep(500 * time.Millisecond)
+// 		select {
+// 		case <-exitChan:
+// 			break
+// 		default:
+// 		}
+// 	}
 
-}
+// }
 
-func main() {
-	wg.Add(1)
-	go f()
-	time.Sleep(3 * time.Second)
-	exitChan <- true
-	wg.Wait()
-}
+// func main() {
+// 	wg.Add(1)
+// 	go f()
+// 	time.Sleep(3 * time.Second)
+// 	exitChan <- true
+// 	wg.Wait()
+// }
 
 // 方法3 使用LOOP跳出循环
 // LOOP:
@@ -69,3 +70,32 @@ func main() {
 // 		default:
 // 		}
 // 	}
+
+//方法4  使用context
+var wg sync.WaitGroup
+
+func f(ctx context.Context) {
+	defer wg.Done()
+	for {
+		fmt.Println("hehe")
+		time.Sleep(500 * time.Millisecond)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+	}
+
+}
+
+func main() {
+	// 创建一个context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go f(ctx)
+	time.Sleep(3 * time.Second)
+	//执行cancel方法，往ctx.Down()这个chan中传递信号
+	cancel()
+	wg.Wait()
+}
