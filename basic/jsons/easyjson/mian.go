1a70a2b115f5769c90cc83f0e7be290514ef8e49
ggvylf <ggvylf@163.com>
2022-12-08 16:51:22 +0800

    update

--- basic/jsons/easyjson/mian.go
+++ basic/jsons/easyjson/mian.go
@@ -1,6 +1,6 @@
 package main
 
-//go get github.com/mailru/easyjson/...
+// go get github.com/mailru/easyjson && go install github.com/mailru/easyjson/...@latest
 // ~/go/bin/easyjson -all *.go
 
 func main() {
---
+++ basic/prof/example/go.mod
@@ -0,0 +1,7 @@
+module github.com/ggvylf/learngo/basic/prof/example
+
+go 1.19
+
+require github.com/mailru/easyjson v0.7.7
+
+require github.com/josharian/intern v1.0.0 // indirect
